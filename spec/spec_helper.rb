b1c62825b2627ffde245ef7c8a472fb71bbd858d
Idyllize <franktrupio@gmail.com>
2014-06-18 09:30:01 -0400

    fix order

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
 
-require 'minitest/spec'
 require 'minitest/autorun'
+require 'minitest/spec'
 require 'minitest-rails'
 require 'minitest-rails-capybara'
