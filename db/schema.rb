6aef842ee65fc5153e68acc078f02c336f190f33
Idyllize <franktrupio@gmail.com>
2014-07-13 19:18:46 -0400

    add pin scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140618154448) do
+ActiveRecord::Schema.define(version: 20140707142728) do
 
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -29,6 +29,12 @@ ActiveRecord::Schema.define(version: 20140618154448) do
 
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+
+  create_table "pins", force: true do |t|
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
